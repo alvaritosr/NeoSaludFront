e0ec06c46ad2cd071f09258d97851c93e63f6de7
Nyk <efernandezjurado@safareyes.es>
2025-05-20 09:35:53 +0200

    Cambiar Contraseña terminado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -19,43 +19,19 @@ export const routes: Routes = [
     loadComponent: () => import('./home/home.component').then((m) => m.HomeComponent),
   },
   {
-    path: 'select-user',
-    loadComponent: () => import('./select-user/select-user.component').then((m) => m.SelectUserComponent),
+    path: 'prescripciones',
+    loadComponent: () => import('./prescripciones/prescripciones.component').then((m) => m.PrescripcionesComponent),
   },
   {
-    path: 'heal-history',
-    loadComponent: () => import('./heal-history/heal-history.component').then((m) => m.HealHistoryComponent),
+    path: 'ver-prescripciones',
+    loadComponent: () => import('./ver-prescripciones/ver-prescripciones.component').then((m) => m.VerPrescripcionesComponent),
   },
   {
-    path: 'medical-appointment',
-    loadComponent: () => import('./medical-appointment/medical-appointment.component').then((m) => m.MedicalAppointmentComponent),
+    path: 'recuperar',
+    loadComponent: () => import('./recuperar-cuenta/recuperar-cuenta.component').then((m) => m.RecuperarCuentaComponent),
   },
   {
-    path: 'chats-selector',
-    loadComponent: () => import('./chats-selector/chats-selector.component').then((m) => m.ChatsSelectorComponent),
-  },
-  {
-    path: 'chat/:id',
-    loadComponent: () => import('./chat/chat.component').then((m) => m.ChatComponent),
-  },
-  {
-    path: 'hcdm-visor',
-    loadComponent: () => import('./hcdm-visor/hcdm-visor.component').then((m) => m.HcdmVisorComponent),
-  },
-  {
-    path: 'calendar',
-    loadComponent: () => import('./calendar/calendar.component').then((m) => m.CalendarComponent),
-  },
-  {
-    path: 'visor-dicom',
-    loadComponent: () => import('./visor-dicom/visor-dicom.component').then((m) => m.VisorDicomComponent),
-  },
-  {
-    path: 'ingresos',
-    loadComponent: () => import('./ingresos/ingresos.component').then((m) => m.IngresosComponent),
-  },
-  {
-    path: 'vacunas',
-    loadComponent: () => import('./vacunas/vacunas.component').then((m) => m.VacunasComponent),
+    path: 'restablecer-contrasena', // Quitamos el parámetro de la ruta
+    loadComponent: () => import('./recuperar-contrasena/recuperar-contrasena.component').then((m) => m.RecuperarContrasenaComponent),
   }
 ];
