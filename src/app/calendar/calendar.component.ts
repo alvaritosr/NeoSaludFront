b8f4aece6ac8299692763fd5633f99c59c6066cb
jcanterla <jcanterlarufino@safareyes.es>
2025-05-29 22:35:07 +0200

    calendario

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import {CommonModule} from "@angular/common";
-import {IonicModule} from "@ionic/angular";
-import {MenuSuperiorComponent} from "../menu-superior/menu-superior.component";
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { MenuSuperiorComponent } from '../menu-superior/menu-superior.component';
 
 @Component({
   selector: 'app-calendar',
@@ -15,4 +15,31 @@ import {MenuSuperiorComponent} from "../menu-superior/menu-superior.component";
   styleUrls: ['./calendar.component.scss'],
 })
 export class CalendarComponent {
+  daysOfWeek = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes'];
+  hours = Array.from({ length: 7 }, (_, i) => `${i + 8}:00`);
+  currentDate = new Date();
+  weekDates: { day: string; date: string }[] = [];
+
+  constructor() {
+    this.calculateWeekDates();
+  }
+
+  calculateWeekDates() {
+    const startOfWeek = new Date(this.currentDate);
+    startOfWeek.setDate(this.currentDate.getDate() - this.currentDate.getDay() + 1); // Lunes
+
+    this.weekDates = this.daysOfWeek.map((day, index) => {
+      const date = new Date(startOfWeek);
+      date.setDate(startOfWeek.getDate() + index);
+      return {
+        day,
+        date: `${date.getDate()} ${date.toLocaleString('es-ES', { month: 'long' })} ${date.getFullYear()}`,
+      };
+    });
+  }
+
+  moveWeek(direction: number) {
+    this.currentDate.setDate(this.currentDate.getDate() + direction * 7);
+    this.calculateWeekDates();
+  }
 }
