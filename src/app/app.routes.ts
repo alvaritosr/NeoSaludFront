02e64a094ca876c29e29f6f065939e40f097f10c
Nyk <efernandezjurado@safareyes.es>
2025-05-29 20:47:25 +0200

    Me duele la cabeza (Prescripción semifuncional)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -31,7 +31,40 @@ export const routes: Routes = [
     loadComponent: () => import('./recuperar-cuenta/recuperar-cuenta.component').then((m) => m.RecuperarCuentaComponent),
   },
   {
-    path: 'restablecer-contrasena', // Quitamos el parámetro de la ruta
+    path: 'restablecer-contrasena',
     loadComponent: () => import('./recuperar-contrasena/recuperar-contrasena.component').then((m) => m.RecuperarContrasenaComponent),
-  }
+  },
+  {
+    path: 'select-user',
+    loadComponent: () => import('./select-user/select-user.component').then((m) => m.SelectUserComponent),
+  },
+  {
+    path: 'heal-history',
+    loadComponent: () => import('./heal-history/heal-history.component').then((m) => m.HealHistoryComponent),
+  },
+  {
+    path: 'medical-appointment',
+    loadComponent: () => import('./medical-appointment/medical-appointment.component').then((m) => m.MedicalAppointmentComponent),
+  },
+  {
+    path: 'chats-selector',
+    loadComponent: () => import('./chats-selector/chats-selector.component').then((m) => m.ChatsSelectorComponent),
+  },
+  {
+    path: 'chat',
+    loadComponent: () => import('./chat/chat.component').then((m) => m.ChatComponent),
+  },
+  {
+    path: 'hcdm-visor',
+    loadComponent: () => import('./hcdm-visor/hcdm-visor.component').then((m) => m.HcdmVisorComponent),
+  },
+  {
+    path: 'calendar',
+    loadComponent: () => import('./calendar/calendar.component').then((m) => m.CalendarComponent),
+  },
+  {
+    path: 'visor-dicom',
+    loadComponent: () => import('./visor-dicom/visor-dicom.component').then((m) => m.VisorDicomComponent),
+  },
+
 ];
