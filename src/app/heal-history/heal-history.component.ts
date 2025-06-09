329ae96ec3417181f2ca233e97ebbf5714763334
jcanterla <jcanterlarufino@safareyes.es>
2025-06-09 18:37:22 +0200

    merge con rama alvaro

--- src/app/heal-history/heal-history.component.ts
+++ src/app/heal-history/heal-history.component.ts
@@ -2,9 +2,9 @@ import {Component, OnInit} from '@angular/core';
 import {IonicModule, MenuController} from "@ionic/angular";
 import {MenuSuperiorComponent} from "../menu-superior/menu-superior.component";
 import { MedicoService } from '../services/medico.service';
-import { ActivatedRoute } from '@angular/router';
+import {ActivatedRoute, Router, RouterLink} from '@angular/router';
 import { AuthService } from '../services/auth.service';
-import {NgForOf, NgIf} from '@angular/common';
+import {DatePipe, NgForOf, NgIf} from '@angular/common';
 import {AntecedentesService} from "../services/antecedentes.service";
 import {AlergiasService} from "../services/alergias.service";
 import {AnalisisService} from "../services/analisis.service";
@@ -15,11 +15,13 @@ import {HabitosVidaService} from "../services/habitos-vida.service";
   selector: 'app-heal-history',
   templateUrl: './heal-history.component.html',
   styleUrls: ['./heal-history.component.scss'],
+  providers: [DatePipe],
   imports: [
     IonicModule,
     MenuSuperiorComponent,
     NgIf,
-    NgForOf
+    NgForOf,
+    RouterLink
   ],
 })
 export class HealHistoryComponent implements OnInit {
@@ -28,11 +30,14 @@ export class HealHistoryComponent implements OnInit {
   alergias: string[] = [];
   analisis: string[] = [];
   antecedentes: string[] = [];
+  consultas: any[] = [];
+
   detalleAlergias: any;
   detalleAnalisis: any;
   detalleAntecedente: any;
   habitos: string[] = [];
   detalleHabito: any;
+  detalleConsulta: any;
 
   constructor(
     private medicoService: MedicoService,
@@ -42,6 +47,8 @@ export class HealHistoryComponent implements OnInit {
     private route: ActivatedRoute,
     private authService: AuthService,
     private habitosVidaService: HabitosVidaService,
+    private router: Router,
+    private datePipe: DatePipe
   ) {}
 
   ngOnInit() {
@@ -52,6 +59,7 @@ export class HealHistoryComponent implements OnInit {
       if (nh && this.nombreMedico) {
         this.medicoService.verDetallePaciente(nh, this.nombreMedico).subscribe(data => {
           this.paciente = data;
+          this.paciente.fecha = this.datePipe.transform(this.paciente.fecha, 'dd/MM/yyyy HH:mm');
         });
 
         this.alergiasService.verAlergias(nh, this.nombreMedico).subscribe(data => {
@@ -64,6 +72,13 @@ export class HealHistoryComponent implements OnInit {
 
         this.antecedentesService.verAntecedentesFamiliares(nh, this.nombreMedico).subscribe(data => {
           this.antecedentes = data;
+        });
+
+        this.medicoService.verConsultas(nh, this.nombreMedico).subscribe(data => {
+          this.consultas = data;
+          this.consultas.forEach(consulta => {
+            consulta.fechaConsulta = this.datePipe.transform(consulta.fechaConsulta, 'dd/MM/yyyy HH:mm');
+          });
         });
         this.habitosVidaService.obtenerTiposDeHabitosDeVida(nh, this.nombreMedico).subscribe(data => {
           this.habitos = data;
@@ -79,6 +94,7 @@ export class HealHistoryComponent implements OnInit {
       this.detalleAnalisis = null;
       this.detalleAntecedente = null;
       this.detalleHabito = null;
+      this.detalleConsulta = null;
       this.alergiasService.verAlergiasDetalles(nh, nombreAlergia, usernameMedico).subscribe((data: any) => {
         this.detalleAlergias = data;
       });
@@ -90,6 +106,7 @@ export class HealHistoryComponent implements OnInit {
     if (nh) {
       this.detalleAlergias = null;
       this.detalleAntecedente = null;
+      this.detalleConsulta = null;
       this.detalleHabito = null;
       this.analisisService.verAnaliticasDetalle(nh, nombreAnalisis).subscribe((data: any) => {
         this.detalleAnalisis = data;
@@ -103,9 +120,25 @@ export class HealHistoryComponent implements OnInit {
     if (nh && usernameMedico) {
       this.detalleAlergias = null;
       this.detalleAnalisis = null;
+      this.detalleConsulta = null;
       this.detalleHabito = null;
       this.antecedentesService.verAntecedenteFamiliarDetalle(nh, nombreAntecedente, usernameMedico).subscribe((data: any) => {
         this.detalleAntecedente = data;
+      });
+    }
+  }
+
+  cargarDetalleConsulta(idConsulta: number) {
+    const nh = this.paciente?.nh;
+    const usernameMedico = this.nombreMedico;
+    if (nh && usernameMedico) {
+      this.detalleAlergias = null;
+      this.detalleAnalisis = null;
+      this.detalleAntecedente = null;
+      this.medicoService.verDetalleConsulta(nh, idConsulta, usernameMedico).subscribe((data: any) => {
+        this.detalleConsulta = data;
+        this.detalleConsulta.fechaConsulta = this.datePipe.transform(this.detalleConsulta.fechaConsulta, 'dd/MM/yyyy');
+        this.detalleConsulta.horaConsulta = this.datePipe.transform(this.detalleConsulta.fechaConsulta, 'HH:mm');
       });
     }
   }
