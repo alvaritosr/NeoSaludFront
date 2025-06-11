34edb34a11f6326e71e4ffacb3bcd01e1519e78e
jcanterla <jcanterlarufino@safareyes.es>
2025-06-11 21:01:50 +0200

    radiografia

--- src/app/visor-dicom/visor-dicom.component.ts
+++ src/app/visor-dicom/visor-dicom.component.ts
@@ -143,7 +143,7 @@ export class VisorDicomComponent implements OnInit, AfterViewInit {
     const fileName = this.dicomFiles[this.currentIndex].fileName;
     const carpeta = nombreCarpeta || '301D3YOC'; // fallback por si acaso
 
-    const imageId = `wadouri:https://neosaludback.onrender.com/tac/${carpeta}/${fileName}`;
+    const imageId = `wadouri:/https://neosaludback.onrender.com/tac/${carpeta}/${fileName}`;
 
     cornerstone.loadImage(imageId).then((image: any) => {
       cornerstone.displayImage(this.dicomImage.nativeElement, image);
