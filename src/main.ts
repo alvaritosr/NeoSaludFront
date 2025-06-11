206fe12d4ce121944c50a90a328048bf504d174e
jcanterla <jcanterlarufino@safareyes.es>
2025-06-11 02:09:47 +0200

    version hammer

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
+import 'hammerjs';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
-import {provideHttpClient} from "@angular/common/http";
+import { provideHttpClient } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
   providers: [
