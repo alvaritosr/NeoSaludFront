c8e67b450bb1fc22ce76e831b214601549f36afe
Nyk <efernandezjurado@safareyes.es>
2025-06-11 23:29:31 +0200

    añadidos más toast a select-user.component.ts

--- src/app/select-user/select-user.component.ts
+++ src/app/select-user/select-user.component.ts
@@ -62,8 +62,8 @@ export class SelectUserComponent {
     this.pacienteService.buscarPacientes(filteredParams).subscribe(
       (data) => {
         this.pacientes = data;
-        if (!this.pacientes || this.pacientes.length === 0) {
-          this.toastErrorService.presentToast('No se encontraron pacientes', 3000);
+        if (Object.keys(filteredParams).length > 0 && (!this.pacientes || this.pacientes.length === 0)) {
+          this.toastErrorService.presentToast('Paciente no encontrado', 3000);
         }
       },
       (error) => {
